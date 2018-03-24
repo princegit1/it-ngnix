716a4db5c8658fabfaf0d5279e3c3acb609d0d40
Prince Kumar <Prince.Kumar@intoday.com>
2018-03-24 13:37:19 +0530

    first Commit

---
+++ map2017/itgFooter.php
@@ -0,0 +1,31 @@
+ <LINK HREF="stylesheet.css" REL="stylesheet" TYPE="text/css">
+<body background="grfx/bg.gif" leftmargin="0" topmargin="0">
+<table width="780" border="0" cellspacing="0" cellpadding="0" align="center">
+  <tbody><tr> 
+    <td height="20" align="center" valign="middle" bgcolor="#000000"> 
+      <table width="871" height="25" border="0" cellpadding="0" cellspacing="0">
+        <tbody><tr> 
+          <td width="90" align="center"> 
+            <p><a href="http://www.indiatodaygroup.com/new-site/index.html" class="LINK2" target="_blank"><font color="#FFFFFF">ABOUT 
+              US</font></a></p>          </td>
+          <td width="10"><img src="grfx/bottom.gif" width="2" height="12"></td>
+          <td width="88"><a href="http://www.syndicationstoday.com" class="LINK2" target="_blank"><font color="#FFFFFF">SYNDICATIONS</font></a></td>
+          <td width="10"><img src="grfx/bottom.gif" width="2" height="12"></td>
+          <td width="83"><a href="http://www.indiatodaygroup.com/new-site/publications.html" class="LINK2" target="_blank"><font color="#FFFFFF">SUBSCRIPTION</font></a></td>
+		    <td width="10"><img src="grfx/bottom.gif" width="2" height="12"></td>
+          <td width="65"><a href="http://www.wecare24x7.in/" class="LINK2" target="_blank"><font color="#FFFFFF">WE CARE</font></a></td>
+          <td width="10"><img src="grfx/bottom.gif" width="2" height="12"></td>
+          <td width="105"><a href="http://specials.indiatoday.com/common/privacypolicy/privacy.phtml" class="LINK2" target="_blank"><font color="#FFFFFF">PRIVACY 
+            POLICY</font> </a></td>
+          <td width="10"><img src="grfx/bottom.gif" width="2" height="12"></td>
+          <td width="80"><a href="http://www.indiatodaygroup.com/contact.php" target="_blank" class="LINK2"><font color="#FFFFFF">CONTACT 
+            US</font></a></td>
+          <td width="10"><img src="grfx/bottom.gif" width="2" height="12"></td>
+         
+          <td width="150"><a href="http://specials.indiatoday.com/common/termsandconditions.phtml" target="_blank" class="LINK2"><font color="#FFFFFF">TERMS AND CONDITIONS</font></a></td>
+ <td width="150"><a href="javascript:void(0)" onclick='javascript:window.open("http://specials.indiatoday.com/specials/popup/advertisewithus.htm", "Advertise with Us", "width=200,height=100")' class="LINK2"><font color="#FFFFFF">TERMS AND CONDITIONS</font></a></td>
+        </tr>
+      </tbody></table>
+    </td>
+  </tr>
+</tbody></table>
